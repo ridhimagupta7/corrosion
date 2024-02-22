07733da050c231db39961de205746c22aa6dcdd7
Ridhima Gupta <107081162+ridhimagupta7@users.noreply.github.com>
2024-02-21 21:54:57 -0600

    Update DataAnalysisTrendlineCode.py

--- DataAnalysisTrendlineCode.py
+++ DataAnalysisTrendlineCode.py
@@ -14,17 +14,13 @@ from sklearn.linear_model import LinearRegression
 x = np.array([956, 923, 895, 936, 961, 1016, 1068, 1092, 1140, 1176, 1208]).reshape((-1, 1))
 y = np.array([0, 24, 45, 70, 120, 170, 216, 286, 335, 383, 431])
 
-# Create new feature array with squared values of x
 x_squared = x ** 2
 
-# Concatenate x_squared with x to create a new feature matrix
 X_new = np.concatenate((x_squared, x), axis=1)
 
-# Create and fit model
 model = LinearRegression()
 model.fit(X_new, y)
 
-# Make prediction
 x_new_value = np.array([[6 ** 2, 6]])  # Square the new x value and reshape to match the feature matrix shape
 y_pred = model.predict(x_new_value)
 
